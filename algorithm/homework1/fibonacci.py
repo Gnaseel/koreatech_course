3d4c5227f3cfd69dcc28f1bd3fcab32de43f6f6e
Gnaseel <hgnaseel@gmail.com>
2022-03-16 04:19:44 +0900

    algorithm1_homework1 update

--- algorithm/homework1/fibonacci.py
+++ algorithm/homework1/fibonacci.py
@@ -1,36 +1,85 @@
-import time
-
+from re import T
+from timeit import default_timer as timer
+# import matplotlib.pyplot as plt
 
 def main():
-    num = 10
+    # num = 30
+    # searchInstance(num, True)
 
-    start = time.time()
-    result1 = iterative_fibonacci(num)
-    end = time.time()
+    time_record=[]
+    fibonacci=[]
+    for i in range(num):
+        time_record.append(searchInstance(i, False))
+        fibonacci.append(i)
+    viz(fibonacci, time_record)
+    return
 
-    print("[Iterative Fibonacci]")
-    print("Num {0} : Fibonacci Number {1}".format(num, result1))
-    print("Elapsed Time: {0:08f}s".format((end - start)))
-    print()
+def searchInstance(num, printmode=False):
+    time = []
 
-    start = time.time()
-    result2 = recursive_fibonacci(num)
-    end = time.time()
+    start = timer()
+    result = iterative_fibonacci(num)
+    end = timer()
+    time.append(end-start)
+    if printmode:
+        print_result(num, result, start, end)
 
+    start = timer()
+    result = recursive_fibonacci(num)
+    end = timer()
+    time.append(end-start)
+    if printmode:
+        print_result(num, result, start, end)
+
+
+    return time
+def print_result(num, result, start, end):
     print("[Recursive Fibonacci]")
-    print("Num {0} : Fibonacci Number {1}".format(num, result2))
+    print("Num {0} : Fibonacci Number {1}".format(num, result))
     print("Elapsed Time: {0:08f}s".format((end - start)))
     print()
 
+def viz(x, y):
+    axis_list = [[],[], []]
+    legend_name = ["iterative_fibonacci", "recursive_fibonacci", "f(x) = n^2 * scale(10^-10)"]
 
+    
+    for idx, item in enumerate(y):
+        axis_list[0].append(item[0])
+        axis_list[1].append(item[1])
+        axis_list[2].append(pow(2, idx)/10000000000)
+    for idx, item in enumerate(axis_list):
+        if idx==2:
+            # plt.plot(x, item, label=legend_name[idx], linestyle="--")
+            continue
+        else:
+        #     continue
+            plt.plot(x, item, label=legend_name[idx], linewidth=5)
+    
+    plt.rc('font', size=10) 
+    plt.legend(loc = 'upper left')
+    plt.title("Raw result")
+    plt.xlabel("fibonacci num", fontsize=7) 
+    plt.ylabel("time (ms)", fontsize=7) 
+
+    plt.show()
+    return
+
+# 반복 피보나치
 def iterative_fibonacci(num):
-    pass # pass 라는 본 라인은 추후 삭제하세요.    
-    # 코딩을 추가하세요.
+    item1=0
+    item2=1
+    for i in range(num):    # 입력받은 횟수만큼 반복
+        item1, item2 =item2, item1+item2
+    return item1
 
-
+# 재귀 피보나치
 def recursive_fibonacci(num):
-    pass # pass 라는 본 라인은 추후 삭제하세요.    
-    # 코딩을 추가하세요.
+    if num==0:              # 0일경우 0 반환
+        return 0
+    if num==1 or num==2:    # 첫 번째나 두 번째항일 경우 1 반환
+        return 1
+    return recursive_fibonacci(num-1) + recursive_fibonacci(num-2)
 
 
 if __name__ == "__main__":
--- algorithm/homework1/search.py
+++ algorithm/homework1/search.py
@@ -1,69 +1,115 @@
+from math import ceil
 import random
-import time
-
 from timeit import default_timer as timer
-
+import matplotlib.pyplot as plt
 def main():
-    num = 1_000
-    s = []
 
+    # time = searchInstance(num = 1000000, s=[], printmode=True)
+    # print(time)
+    # return
+    search_count_record=[]
+    time_record=[]
+    search_range = 1000000
+    for i in range(1, search_range, 1):
+        search_count_record.append(i)
+        time_record.append(searchInstance(num = i, s=[]))
+    viz(search_count_record, time_record)
+    return
+
+def searchInstance(num, s, printmode=False):
+    time = []
     for value in range(num):
         s.append(random.randint(0, num))
 
     key = random.randint(0, num)
 
     start = timer()
-    # start_time=time.time()
     location = sequential_search(s, key)
     end = timer()
-    # end_time=time.time()
-
-    #print(s)
-    print("[Sequential Search Result]")
-    print("Key value {0}: location {1}".format(key, location))
-    print("Elapsed Time: {}ms".format((end - start)))
-    print("===================================")
-    print("Start_timer Time: {}ms".format(start))
-    print("End_timer Time: {}ms".format(end))
-    # print("Start Time: {}ms".format(start_time))
-    # print("End Time: {}ms".format(end_time))
-    print("===================================")
-    print()
+    time.append((end - start) * 1_000)
+    
+    if printmode:
+        print_result(key, location, s, start, end)
 
     s.sort()
 
-    start = time.time()
+    start = timer()
     location = binary_search(s, key)
-    end = time.time()
+    end = timer()
+    time.append((end - start) * 1_000)
 
-    #print(s)
-    print("[Binary Search Result]")
-    print("Key value {0}: location {1}".format(key, location))
-    print("Elapsed Time: {0:0.8f}ms".format((end - start) * 1_000))
-    print()
-
-    start = time.time()
+    if printmode:
+        print_result(key, location, s, start, end)
+    start = timer()
     location = recursive_binary_search(s, key, 0, num - 1)
-    end = time.time()
+    end = timer()
+    time.append((end - start) * 1_000)
 
-    #print(s)
-    print("[Recursive Binary Search Result]")
-    print("Key value {0}: location {1}".format(key, location))
-    print("Elapsed Time: {0:0.8f}ms".format((end - start) * 1_000))
+    if printmode:
+        print_result(key, location, s, start, end)
+
+    return time
+
+def print_result(key, location, s, start, end):
+    print("[Sequential Search Result]")
+    print("Key value {0}: location {1}: check {2}".format(key, location, s[location]))
+    print("Elapsed Time: {0:0.8f}msms".format((end - start) * 1_000))
     print()
+    return
 
+def MAfilter(list, window_size):
+    re_list=[]
+    for i in range(len(list)-window_size):
+        list[i] = sum(list[i:i+window_size])/window_size
+    for i in range(len(list)-window_size, len(list)):
+        list[i] = sum(list[len(list)-window_size:len(list)])/window_size
+    return list
+
+def viz(x, y):
+    axis_list = [[],[],[]]
+    legend_name = ["sequential_search", "binary_search", "recursive_binary_search"]
+
+    
+    for item in y:
+        axis_list[0].append(item[0])
+        axis_list[1].append(item[1])
+        axis_list[2].append(item[2])
+    for idx, item in enumerate(axis_list):
+        # if idx==0:
+        #     continue
+        plt.subplot(1,2,1)
+        plt.plot(x, item, label=legend_name[idx])
+        plt.subplot(1,2,2)
+        item = MAfilter(item, 80)
+        plt.plot(x, item, label=legend_name[idx])
+        if idx==1000 or idx == 10000 or idx == 100000 or idx == 1000000:
+            print("idx = {} time = {}".format(idx, item))
+    
+    plt.subplot(1,2,1)
+    plt.rc('font', size=20) 
+    plt.legend(loc = 'upper left')
+    plt.title("Raw result")
+    plt.xlabel("search size", fontsize=15) 
+    plt.ylabel("time (ms)", fontsize=15) 
+    plt.subplot(1,2,2)
+    plt.rc('font', size=20) 
+    plt.legend(loc = 'upper left')
+    plt.title("Moving average filter (window = 80)")
+    plt.xlabel("search size", fontsize=15) 
+    plt.ylabel("time (ms)", fontsize=15) 
+    plt.show()
+    return
 
 def sequential_search(s, key):
     num = len(s)
     location = 0
+
     # 코딩을 추가하세요.
 
+    # 순차적으로 리스트 전체를 검색한다.
     for idx, item in enumerate(s):
         if item == key:
-            # print("key is {} {}".format(key, item))
-            # print("key is {}".format(idx))
             return idx
-    print("No key in list")
     return -1
 
 def binary_search(s, key):
@@ -71,13 +117,15 @@ def binary_search(s, key):
     low = 0
     high = num - 1
     location = -1
+
     # 코딩을 추가하세요.
+
+    # 리스트에 키가 존재하지 않을때까지 검색
     while low <= high:
         mid = (low + high) // 2
         if s[mid] > key:
             high = mid - 1
         if s[mid] == key:
-            print("key = {} {}".format(key, s[mid]))
             return mid
         if s[mid] < key:
             low = mid + 1
@@ -89,7 +137,17 @@ def recursive_binary_search(s, key, low, high):
     mid = round((low + high) / 2)
 
     # 코딩을 추가하세요.
+    # round 함수는 사사오입 성질 때문에 홀짝에 대해 결과가 다름. 사용 불편하므로 일관성있는 ceil 사용
+    mid = ceil((low + high) / 2)
 
+    if s[mid] == key:   # 리스트에서 키 발견시 리턴
+        return mid
+    if low == high:     # 리스트에 키가 없는 경우
+        return -1
+    if s[mid] > key:    # 키가 작은경우 리스트의 전반부 검색
+        return recursive_binary_search(s, key, low, mid-1)
+    if s[mid] < key:    # 키가 큰 경우 리스트의 후반부 검색
+        return recursive_binary_search(s, key, mid, high)
 
 if __name__ == "__main__":
     main()
